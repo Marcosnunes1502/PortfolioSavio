85bf1518731cc5aa43a37b2c96e56e5128995d47
Fernando Alves Rufino <fernandorufinoalves@gmail.com>
2022-11-14 17:47:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-const header = document.querySelector("header")
-window.addEventListener("scroll", function() {
-  header.classList.toggle("sticky", window.scrollY > 100)
-})
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 100);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('open');
+};
+
+const list = document.querySelectorAll('[name="nav-a"]')
+
+function activeLink(){
+	list.forEach((item) =>
+	item.classList.remove('active'))
+	this.classList.add('active')
+}
+
+list.forEach((item) =>
+item.addEventListener('click', activeLink))
 
